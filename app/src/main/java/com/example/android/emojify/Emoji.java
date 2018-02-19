449fe66608dbb1d0d74dd5c0a661227dbf548b12
Avi Gagel <avigagel@Avis-MacBook-Pro.local>
2018-02-19 20:12:28 +0200

    Create Enum java class for facial expressions

---
+++ app/src/main/java/com/example/android/emojify/Emoji.java
@@ -0,0 +1,11 @@
+package com.example.android.emojify;
+
+/**
+ * Created by avigagel on 19/02/2018.
+ */
+
+public enum Emoji {
+
+    SMILING, FROWNING, LEFT_WINK, RIGHT_WINK, LEFT_WINK_FROWNING, RIGHT_WINK_FROWNING,
+    CLOSED_EYE_SMILING, CLOSED_EYE_FROWNING, LEFT_WINK_SMILING, RIGHT_WINK_SMILING
+}
