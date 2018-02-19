b0a44c26a4a8f95cce06cd9e3e43a6a54c29a781
Avi Gagel <avigagel@Avis-MacBook-Pro.local>
2018-02-19 19:13:44 +0200

    Add face detection class with face detection method, and hook up to MainActivity

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -1,8 +1,37 @@
 package com.example.android.emojify;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
 /**
  * Created by avigagel on 19/02/2018.
  */
 
 public class Emojifier {
+
+
+    static void detectFaces(Bitmap resampledBitmap, Context context) {
+
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.NO_LANDMARKS)
+                .build();
+
+        Frame frame = new Frame.Builder().setBitmap(resampledBitmap).build();
+
+        SparseArray<Face> faces = detector.detect(frame);
+
+        if (faces.size() == 0) {
+            Toast.makeText(context, "no faces in image", Toast.LENGTH_SHORT).show();
+        } else {
+            Log.i("Emojifier: ", "number of faces in Bitmap is: " + faces.size());
+        }
+    }
 }
